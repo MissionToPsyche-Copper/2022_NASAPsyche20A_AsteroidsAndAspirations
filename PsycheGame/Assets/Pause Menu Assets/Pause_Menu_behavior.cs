7c1d2cac654355c999b7e8d53f3ba564cfb49d81
Jzhan444 <Jzhan444@asu.edu>
2022-03-28 00:09:52 -0700

    How to Play, Pause, and Title scene updated

---
+++ PsycheGame/Assets/Pause Menu Assets/Pause_Menu_behavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Menu_behavior : MonoBehaviour
+{
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+    public void loadMainMenu()
+    {
+        Time.timeScale = 1f;
+        //SceneManager.LoadScene("");
+    }
+    public void howToPlay()
+    {
+        //SceneManager.LoadScene("");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
